21a22f18ba17b5f874b4e38f9cb890704b1ac406
Everton Jr <evertonse.junior@gmail.com>
2023-02-13 03:52:29 -0300

    more test files

--- src/elipses/Main.java
+++ src/elipses/Main.java
@@ -7,32 +7,25 @@ import elipses.node.*;
 
 public class Main
 {
-  String[] test_files = new String[]{""};
+  static String[] test_files = new String[] {
+    //"test/grupo10/*",
+    "test/grupo10/codigo1_grupo10.elip",
+    //"test/etapa1/code1.elip", 
+    //"test/etapa1/code2.elip", 
+    //"test/etapa1/code3.elip", 
+  };
+
 	public static void main(String[] args)
 	{
-		try
-		{
-<<<<<<< HEAD
-          Debug.debug("test/etapa1/code1.elip");
-          Debug.debug("test/etapa1/code2.elip");
-          Debug.debug("test/etapa1/code3.elip");
+		try {
+      for (String elip_file : test_files) {
+        Debug.debug(elip_file);
+      }
 		}
+
 		catch(Exception e) {
             e.printStackTrace();
     	    System.out.println(e.getClass() + e.getMessage());
-=======
-			String arquivo = "test/number.elip";
-			Lexer lexer = new Lexer(
-				new PushbackReader(
-					new FileReader(arquivo),1024
-				)
-			); 
-			Debug.lexer(lexer);
-		}
-		catch(Exception e) {
-			e.printStackTrace();
-			System.out.println(e.getClass() + e.getMessage());
->>>>>>> 1567e28fccf00d5e7e9be2809b008684a0aba2c4
 		}
 	}
 }
@@ -41,17 +34,17 @@ class Utils {
 	public static String pretify(Token token) {
 		String token_pos = new String("[" + token.getLine() + "," + token.getPos()+ "]:"); 
 		String token_tuple = new String(
-	    	" < "
+	    	"<"
 	    	+ token.getClass().getSimpleName()
-	    	+ ", "   + token.toString()
+	    	+ ", "   + token.toString().stripTrailing()
 	    	+ ">"
 	    );
+
 		return String.format("Token %10s %s", token_pos, token_tuple);
 	}
 }
 
 class Debug {
-<<<<<<< HEAD
   public static 
   void lexer(Lexer lexer) throws LexerException, IOException {
     try {
@@ -71,6 +64,17 @@ class Debug {
   void debug(String file)
 	{
     System.out.println("\nChecking Lexer for file: " + file);
+    if (file.endsWith("*")) {
+      File folder = new File(file.replace("*", ""));
+      File[] child_files = folder.listFiles();
+      for (File cf : child_files) {
+        if (cf.isFile()) {
+          Debug.debug(cf.getPath());
+        }
+      }
+      return;
+    }
+
 		try
 		{
 			String arquivo = file;
@@ -84,21 +88,6 @@ class Debug {
 		catch(Exception e) {
       e.printStackTrace();
 			System.out.println(e.getClass() + e.getMessage());
-=======
-	public static void lexer(Lexer lexer) throws LexerException, IOException {
-		try 
-		{
-			Token token;
-			while(!((token = lexer.next()) instanceof EOF)) {
-	          if (token instanceof TBlank) 
-	            continue;
-	          System.out.println(Utils.pretify(token));
-			}
-		}
-		catch (Exception e) {
-			System.out.println("\nError: " + e.getClass() + e.getMessage() + "\nContinuing with next tokens ...\n");
-			Debug.lexer(lexer);
->>>>>>> 1567e28fccf00d5e7e9be2809b008684a0aba2c4
 		}
 	}
 }
--- test/test.c
+++ test/test.c
@@ -7,3 +7,4 @@ int main(int argc, char const *argv[])
 
   return 0;
 }
+//
